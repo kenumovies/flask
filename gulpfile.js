c28a285c58ef2239eb7e9d6ecf5ff977cc22fb30
nikhilpi <nikhilpai2016@u.northwestern.edu>
2015-11-22 01:14:49 -0600

    removed react

--- gulpfile.js
+++ gulpfile.js
@@ -15,8 +15,8 @@ var gulp = require('gulp'),
 // tasks
 
 gulp.task('transform', function () {
-  return gulp.src('./project/static/scripts/jsx/**/*.js')
-    .pipe(browserify({transform: ['reactify'],entries: ['./main.js']}))
+  return gulp.src('./project/static/scripts/source/**/*.js')
+    .pipe(browserify())
     .pipe(gulp.dest('./project/static/scripts/js'))
     .pipe(size());
 });
