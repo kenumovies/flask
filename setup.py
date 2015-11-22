318a5c43ed4b2bf8b0ae601694553338041b1d31
nikhilpi <nikhilpai2016@u.northwestern.edu>
2015-11-22 00:29:42 -0600

    gulp js min working

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-    # ...,
-    setup_requires=['libsass >= 0.6.0'],
-    sass_manifests={
-        'project': ('static/sass', 'static/css', '/static/css')
-    }
-)
