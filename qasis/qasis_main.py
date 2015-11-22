e74ffdaf58e3567e0e061fd46b214b1f9ab3e6d5
ngparas <nickgp@gmail.com>
2015-11-21 21:41:04 -0600

    made main wrapper

---
+++ qasis/qasis_main.py
@@ -0,0 +1,12 @@
+from tfidf_scores import *
+from make_corpus import *
+from parse_query import *
+from db_access import *
+
+#take string query, return list of tuples
+def main(q, actorList, directorList):
+	#get keywords
+	kw = parseQuery(q)
+	#get docs (for now, ignore actors and directors)
+	docs = query_db("SELECT imdb_id, word_list FROM preprocessed_movies;")
+	return rankDocs(kw, docs)
